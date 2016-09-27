c9aa3f5b778aba197c1924c547a3c821549a159c
Ellen Arvidsson (r@g.n.n) <ellen.arvidsson@gmail.com>
2016-09-27 21:30:58 +0200

    Add local footer.php to adjust the copyright notice.
    The theme is GPL and stating a (c) is not very helpful. The most texts
    and images is CC-BY-SA or something similar (or should be). Having a
    CC-BY-SA notice is a good middle ground where the user can discover
    the nature of sharing culture.

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+	<footer id="colophon" role="contentinfo">
+		<div id="site-generator">
+
+			<?php _e('', 'magazino'); ?><a href="<?php echo esc_url( __( 'https://creativecommons.org/licenses/by-sa/3.0/', 'magazino' ) ); ?>"><?php _e('CC BY-SA 3.0', 'magazino'); ?></a>
+            
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #container -->
+
+<?php wp_footer(); ?> 
+
+</body>
+</html>
